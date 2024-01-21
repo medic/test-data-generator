635d95c75bb34820018d73ad438fb63aca3ce3b0
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-01-21 14:57:34 +0700

    useful error on cli input

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
+import { cli } from './cli.js';
 import { Docs } from './docs.js';
-import * as path from 'node:path';
 import { DocDesign } from './doc-design.js';
 import { context } from './design-context.js';
 
 (async function() {
   try {
-    const args = process.argv.slice(2);
-    const designScriptPath = path.resolve(args[0]);
+    const designScriptPath = cli.getInputFilePath();
     const getDesign: DocDesign = (await import(designScriptPath)).default;
     Docs.createDocs(getDesign(context.get()));
   } catch (error) {
---
+++ tests/cli.spec.ts
@@ -0,0 +1,77 @@
+import { resolve } from 'node:path';
+import { expect, assert } from 'chai';
+import * as Sinon from 'sinon';
+import { restore, stub } from 'sinon';
+import { cli } from '../src/cli.js';
+
+describe('cli', () => {
+  let argvStub: Sinon.SinonStub;
+
+  beforeEach(() => {
+    argvStub = stub(process, 'argv');
+  });
+
+  afterEach(() => restore());
+
+  describe('getInputFilePath', () => {
+    it('should return the design file path successfully', () => {
+      try {
+        argvStub.get(() => [ 'node-path', 'application-entry', 'tests/files/empty-design-file.js' ]);
+
+        const path = cli.getInputFilePath();
+
+        expect(path).to.equal(resolve('tests/files/empty-design-file.js'));
+      } catch (error) {
+        assert.fail('Should have not thrown an error');
+      }
+    });
+
+    it('should throw an error when design file does not exist in path', () => {
+      try {
+        argvStub.get(() => [ 'node-path', 'application-entry', 'tests/files/no-existing.js' ]);
+
+        cli.getInputFilePath();
+
+        assert.fail('Should have thrown an error');
+      } catch (error) {
+        expect(error?.message).to.equal(
+          'The design file does not exist in the specified location. Verify the path is correct.'
+        );
+      }
+    });
+
+    [
+      'folder/file-no-extension',
+      'folder/file-wrong-extension.svg',
+      'file-wrong-extension.ts',
+    ].forEach(filePath => {
+      it('should throw an error when the design file path has the wrong extension', () => {
+        try {
+          argvStub.get(() => [ 'node-path', 'application-entry', filePath ]);
+
+          cli.getInputFilePath();
+
+          assert.fail('Should have thrown an error');
+        } catch (error) {
+          expect(error?.message).to.equal(
+            'The design file is not a JavaScript file. Retry using a file with extension ending in .js'
+          );
+        }
+      });
+    });
+
+    it('should throw an error when design file path is not provided', () => {
+      try {
+        argvStub.get(() => [ 'node-path', 'application-entry' ]);
+
+        cli.getInputFilePath();
+
+        assert.fail('Should have thrown an error');
+      } catch (error) {
+        expect(error?.message).to.equal(
+          'No path to the design file provided. Expected: npm run generate *path_to_your_custom_design_file*'
+        );
+      }
+    });
+  });
+});
